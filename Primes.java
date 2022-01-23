fe52461759d9157aacf3276538b0c1741083a080
Neo <neocamanga@knights.ucf.edu>
2022-01-23 01:12:52 -0500

    working version with commented out ideas

--- Primes.java
+++ Primes.java
@@ -1,21 +1,46 @@
-class Primes
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class Primes implements Runnable
 {
+    public static final long UP_TO = (long)1E8;
+    public static final int MAX_NUM_THREADS = 8;
+    public static final long MAX_POOL = (long)1E7;
+
+
+    public static final AtomicLong counter = new AtomicLong(1);
+    public static final AtomicLong totalSum = new AtomicLong(2);
+    public static final AtomicLong currentNum = new AtomicLong(3);
+
     public static void main(String[] args)
     {
-        System.out.println("Hello");
+        // int coreCount = Runtime.getRuntime().availableProcessors();
+        // System.out.println(coreCount);
+
         findPrimes();
+        // Primes p = new Primes();
+        // Thread th = new Thread(p);
+        // th.start();
+    }
+
+    public void run()
+    {
+        // System.out.println("Running thread!");
     }
 
     private static void findPrimes()
     {
         final long startTime = System.currentTimeMillis();
-        oneThreadPrime((long)1E8);
+        // oneThreadPrime(UP_TO);
+        multiThreadPrime(UP_TO);
         final long endTime = System.currentTimeMillis();
 
         System.out.println("Total execution time: " + (endTime - startTime));
     }
 
-    private static boolean isPrime(long p)
+    public static boolean isPrime(long p)
     {
         for (long i = 3; i <= Math.pow(p, 0.5); i += 2)
             if ((p % i) == 0)
@@ -26,7 +51,7 @@ class Primes
 
     private static long oneThreadPrime(long n)
     {
-        System.out.println(n);
+        // System.out.println(n);
         
         if (n < 2) return 0;
         long sum = 2;
@@ -43,5 +68,77 @@ class Primes
         System.out.println("sum: " + sum);
         System.out.println("count: " + count);
         return sum;
+    }
+
+    private static long multiThreadPrime(long n)
+    {
+        // System.out.println(n);
+
+        if (n < 2) return 0;
+
+        // ExecutorService service = Executors.newFixedThreadPool(MAX_NUM_THREADS);
+
+        // for (long i = 3; i <= n; i += 2)
+        // while (currentNum.getAcquire() <= (n / 4))
+        // {
+        //     service.submit(new TestPrimes(currentNum.getAcquire()));
+        //     currentNum.addAndGet(2);
+        // }
+        // service.shutdown();
+
+        // try {
+        //     service.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+        // } catch (InterruptedException e) {
+        //     System.err.println("Error: " + e);
+        //     return -1L;
+        // }
+        for (long i = MAX_POOL; i <= n; i += MAX_POOL)
+            loop(i);
+
+        System.out.println("totalSum: " + totalSum);
+        System.out.println("counter: " + counter);
+        return totalSum.getAcquire();
+    }
+
+    public static void loop(long n)
+    {
+        ExecutorService service = Executors.newFixedThreadPool(MAX_NUM_THREADS);
+
+        while (currentNum.getAcquire() <= n)
+        {
+            service.submit(new TestPrimes(currentNum.getAcquire()));
+            currentNum.addAndGet(2);
+        }
+        service.shutdown();
+
+        try {
+            service.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+        } catch (InterruptedException e) {
+            System.err.println("Error: " + e);
+        }
+    }
+}
+
+class TestPrimes extends Primes
+{
+    public long cur;
+
+    public TestPrimes(long i)
+    {
+        cur = i;
+        // System.out.println("log: " + cur);
+    }
+
+    public void run()
+    {
+        // System.out.println("run: " + cur);
+
+        // System.out.println("Running thread!");
+        if (isPrime(cur))
+        {
+            // System.out.print(cur + ", ");
+            totalSum.addAndGet(cur);
+            counter.incrementAndGet();
+        }
     }
 }
--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+# How to Run the Program:
+On the command prompt, go to the directory which contains Primes.java, then run:
+java Primes.java
+
+
 # 8-thread-prime-find
 Your non-technical manager assigns you the task to find all primes between 1 and 10<sup>8</sup>.  The assumption is that your company is going to use a parallel machine that supports eight concurrent threads. Thus, in your design you should plan to spawn 8 threads that will perform the necessary computation. Your boss does not have a strong technical background but she is a reasonable person. Therefore, she expects to see that the work is distributed such that the computational execution time is approximately equivalent among the threads.
 
@@ -22,13 +27,22 @@ Please print the following output to a file named primes.txt:
 
 
 
-# How to Run the Program:
-java Primes.java
-
-
 # Resources Used:
-<https://stackoverflow.com/questions/17416949/how-to-get-an-integer-value-that-is-greater-than-the-maximum-integer-value>
-<https://stackoverflow.com/questions/21034955/when-to-use-long-vs-long-in-java>
-<https://stackoverflow.com/questions/2572868/how-to-time-java-program-execution-speed>
-<https://stackoverflow.com/questions/1590831/safely-casting-long-to-int-in-java>
-<https://stackoverflow.com/questions/7093186/the-literal-xyz-of-type-int-is-out-of-range/7093207>
+* <https://primes.utm.edu/howmany.html>
+* <https://stackoverflow.com/questions/17416949/how-to-get-an-integer-value-that-is-greater-than-the-maximum-integer-value>
+* <https://stackoverflow.com/questions/21034955/when-to-use-long-vs-long-in-java>
+* <https://stackoverflow.com/questions/2572868/how-to-time-java-program-execution-speed>
+* <https://stackoverflow.com/questions/1590831/safely-casting-long-to-int-in-java>
+* <https://stackoverflow.com/questions/7093186/the-literal-xyz-of-type-int-is-out-of-range/7093207>
+* <https://www.w3schools.com/java/java_threads.asp>
+* <https://stackoverflow.com/questions/541487/implements-runnable-vs-extends-thread-in-java>
+* <https://www.baeldung.com/java-thread-lifecycle>
+* <https://www.javatpoint.com/how-to-create-a-thread-in-java>
+* <https://stackoverflow.com/questions/9916264/create-a-simple-queue-with-java-threads>
+* <https://docs.oracle.com/en/java/javase/17/docs/api/java.base/java/util/concurrent/Executors.html#newFixedThreadPool(int)>
+* <https://www.baeldung.com/java-executor-service-tutorial>
+* <https://www.youtube.com/watch?v=6Oo-9Can3H8&list=PLhfHPmPYPPRk6yMrcbfafFGSbE2EPK_A6&index=5>
+* <https://www.baeldung.com/java-atomic-variables>
+* <https://stackoverflow.com/questions/53964366/whats-the-difference-between-getvolatile-and-getacquire>
+* <https://stackoverflow.com/questions/877096/how-can-i-pass-a-parameter-to-a-java-thread>
+* <https://stackoverflow.com/questions/1250643/how-to-wait-for-all-threads-to-finish-using-executorservice>
